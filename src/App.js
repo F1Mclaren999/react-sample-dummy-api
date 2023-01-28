26b29319f33e67d956c13c282318e1098f3f44d3
F1Mclaren999 <64996210+F1Mclaren999@users.noreply.github.com>
2023-01-28 15:02:48 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Popover from '@material-ui/core/PopOver';
+import { makeStyles } from '@material-ui/core/styles';
+
 import './style.css';
 
-const InputRef = React.forwardRef((props, ref) => (
-  <input ref={ref} {...props} />
-));
+const useStyles = makeStyles((theme) => ({
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+}));
 
-export default function App() {
-  const buttonRef = React.useRef('');
+const mockData = {
+  recLoadedCnt1: 3527,
+  recLoadedCnt2: 352,
+};
 
-  const handleClick = () => {
-    buttonRef.current.focus();
+export default function App({ test }) {
+  const [loading, setLoading] = React.useState(true);
+  const [cycleSpecificCnt, setCycleSpecificCnt] = React.useState(test);
+
+  useEffect(() => {
+    console.log('useEffect enter');
+
+    const data = async () => {
+      console.log('data check...');
+      if (loading) {
+        const val = await getData();
+        console.log('val...', val);
+        setCycleSpecificCnt(val.data);
+        setLoading(false);
+      }
+    };
+    data();
+  }, []);
+
+  const getData = () => {
+    const test = { ok: true, data: mockData };
+    // console.log('data...', test);
+    return new Promise((resolve) => {
+      resolve(test);
+    });
   };
 
   return (
     <div>
-      <h1>Simple ForwardRef useCase</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <InputRef ref={buttonRef} /> &nbsp;
-      <button onClick={() => handleClick()}>SendingRef</button>
+      <h3>Simple Mock Api</h3>
+      <h4>CycleSpecific Count</h4>
+      {loading ? 'loading' : cycleSpecificCnt && cycleSpecificCnt.recLoadedCnt1}
     </div>
   );
 }
